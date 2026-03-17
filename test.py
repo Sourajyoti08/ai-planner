b648468349b11957ae08ab28729ce65445d4fff6
Sourajyoti08 <sourajyotidey2008@gmail.com>
2026-03-17 15:51:42 +0530

    Enable debug mode for Flask app

--- test.py
+++ test.py
@@ -43,4 +43,4 @@ def home():
     <pre style="color: white; font-family: Special Elite;">{schedule}</pre>
     """
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
